cb922dae8fda05a28484ce9c271ad36a7edfe4fc
Wlsk <Wlsk@example.com>
2024-04-17 22:30:38 +0900

    브랜치1 예제

---
+++ code_sample.py
@@ -0,0 +1,7 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, World!')
+Hello, World!
+>>> 
+>>> print('Branch sample1')
+Branch sample1
