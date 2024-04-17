f79e37f8cda0f373d2b11e0b16d3a49c50133660
Wlsk <Wlsk@example.com>
2024-04-17 21:58:07 +0900

    수업 예제

--- code_sample.py
+++ code_sample.py
@@ -1,4 +1,4 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print('Hello, World!')
 Hello, World!
